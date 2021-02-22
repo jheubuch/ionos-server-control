e554ef4fdeee1e8903fb683e6e773f9b6bb857b6
Jonas Heubuch <jonas.heubuch@outlook.com>
2021-02-22 20:31:12 +0100

    added Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ionos-server-control';
+  title = 'Ionos Server Control';
 }
